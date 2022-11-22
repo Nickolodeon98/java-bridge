fdf9abd42e766122e65cb43f168ca86408122a0a
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-22 23:47:06 +0900

    feat: 기능 목록의 출력 관련 기능 1 구현 중

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,12 +5,24 @@ package bridge;
  */
 public class OutputView {
 
+    private final BridgeGame bridgeGame;
+
+    public OutputView(BridgeGame bridgeGame) {
+        this.bridgeGame = bridgeGame;
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        int position = bridgeGame.getPointer();
+        System.out.print("[ ");
+        for (int i = 0; i < position-1; i++) {
+            System.out.println();
+        }
+        System.out.print(" ]");
     }
 
     /**
